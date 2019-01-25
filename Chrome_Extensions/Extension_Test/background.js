4cdc5d4c1759392e2dca449f64e11841436330df
wezarkeVo <wezarkevo@gmail.com>
2019-01-25 17:40:19 -0500

    DavLink
    Extens Google

---
+++ Chrome_Extensions/Extension_Test/background.js
@@ -0,0 +1,90 @@
+'use strict';
+(function() {
+    const tabStorage = {};
+    const networkFilters = {
+        urls: [
+            "*://localhost/*"
+        ]
+    };
+
+    chrome.webRequest.onBeforeRequest.addListener((details) => {
+        const { tabId, requestId } = details;
+        if (!tabStorage.hasOwnProperty(tabId)) {
+            return;
+        }
+
+        tabStorage[tabId].requests[requestId] = {
+            requestId: requestId,
+            url: details.url,
+            startTime: details.timeStamp,
+            status: 'pending'
+        };
+        console.log(tabStorage[tabId].requests[requestId]);
+    }, networkFilters,["requestBody"]);
+
+    chrome.webRequest.onCompleted.addListener((details) => {
+        const { tabId, requestId } = details;
+        if (!tabStorage.hasOwnProperty(tabId) || !tabStorage[tabId].requests.hasOwnProperty(requestId)) {
+            return;
+        }
+
+        const request = tabStorage[tabId].requests[requestId];
+
+        Object.assign(request, {
+            endTime: details.timeStamp,
+            requestDuration: details.timeStamp - request.startTime,
+            status: 'complete'
+        });
+        console.log(tabStorage[tabId].requests[details.requestId]);
+    }, networkFilters,["responseHeaders","extraHeaders"]);
+
+    chrome.webRequest.onErrorOccurred.addListener((details)=> {
+        const { tabId, requestId } = details;
+        if (!tabStorage.hasOwnProperty(tabId) || !tabStorage[tabId].requests.hasOwnProperty(requestId)) {
+            return;
+        }
+
+        const request = tabStorage[tabId].requests[requestId];
+        Object.assign(request, {
+            endTime: details.timeStamp,
+            status: 'error',
+        });
+        console.log(tabStorage[tabId].requests[requestId]);
+    }, networkFilters);
+
+    chrome.tabs.onActivated.addListener((tab) => {
+        const tabId = tab ? tab.tabId : chrome.tabs.TAB_ID_NONE;
+        if (!tabStorage.hasOwnProperty(tabId)) {
+            tabStorage[tabId] = {
+                id: tabId,
+                requests: {},
+                registerTime: new Date().getTime()
+            };
+        }
+    });
+    chrome.tabs.onRemoved.addListener((tab) => {
+        const tabId = tab.tabId;
+        if (!tabStorage.hasOwnProperty(tabId)) {
+            return;
+        }
+        tabStorage[tabId] = null;
+    });
+}());
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({
+    color: '#3aa757'
+  }, function() {
+    console.log("The color is green.");
+  });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {
+          urlMatches: '(https|http)://(localhost|127\.0\.0\.1|webappletdes\.davivienda\.loc|webappletlab\.davivienda\.loc|168\.170\.1\.218|168\.170\.2\.225)'
+        },
+      })],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
