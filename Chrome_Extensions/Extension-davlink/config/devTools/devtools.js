622bcbb95d7f7fd2d7d75734255609fe6370b394
wezarkeVo <wezarkevo@gmail.com>
2019-03-17 17:00:07 -0500

    Cambios DavLink
    -Manifest version
    -DevTools log

--- Chrome_Extensions/Extension-davlink/config/devTools/devtools.js
+++ Chrome_Extensions/Extension-davlink/config/devTools/devtools.js
@@ -2,6 +2,6 @@ chrome.devtools.panels.create("DavLink",
     "app/site_media/logo/NetLink-48.png",
     "app/html/panelSniffer/panelSniffer.html",
     function(panel) {
-        console.log(panel);
+        console.log('/**DavLink Iniciado**/');
     }
 );
